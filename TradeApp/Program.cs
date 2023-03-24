c0e4a76e9830ee0edae89c387cc56c8e17039385
Leonel Nobrega <lnobrega@uma.pt>
2023-03-24 10:37:06 +0000

    Add project files.

---
+++ TradeApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace TradeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TradeApp v1.0!");
+
+            try
+            {
+                using var stream = File.OpenRead("TradeData.txt");
+
+                var processor = new TradeProcessor();
+                processor.ProcessTrades( stream );
+                
+            }
+            catch ( Exception ex) 
+            { 
+                Console.WriteLine( ex.Message );
+            }
+        }
+    }
+}
---
+++ TradeApp/TradeRecord.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace TradeApp
+{
+    public class TradeRecord
+    {
+        public string  SourceCurrency      { get; set; }
+        public string  DestinationCurrency { get; set; }
+        public double  Lots                { get; set; }
+        public decimal Price               { get; set; }
+    }
+}
