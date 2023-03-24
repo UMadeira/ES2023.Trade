0ce1b24a937e606db15042aad7fb170e0ba248a1
Leonel Domingos Telo Nobrega <lnobrega@staff.uma.pt>
2023-03-24 14:56:37 +0000

    Phase 1

--- TradeApp/TradeProcessor.cs
+++ TradeApp/TradeProcessor.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Data;
+﻿using System.Data;
 using System.Data.SqlClient;
-using System.Runtime.CompilerServices;
-using System.Diagnostics;
 
 namespace TradeApp
 {
